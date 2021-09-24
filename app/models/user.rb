62881d3001d7f2215c97d3b8fca2097b25781f5b
GIT-DS <darrickshin@gmail.com>
2021-09-24 16:33:16 -0700

    Add User Seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,38 @@ class User < ApplicationRecord
     validates_uniqueness_of :username, :email, :session_token
 
     has_many :reviews,
-        foreign_key: :user_id,
-        class_name :Review
+    foreign_key: :user_id,
+    class_name: :Review
+
+    attr_reader :password
+
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.base64
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.new({
+    username:"alpha",
+    password:"alpha",
+    email:"alpha.io",
+    first_name: 'al',
+    last_name: 'pha',
+    zip_code: 00001
+})
+user2 = User.new({
+    username:"bravo",
+    password:"bravo",
+    email:"bravo.io",
+    first_name: 'bra',
+    last_name: 'vo',
+    zip_code: 00002
+})
+user3 = User.new({
+    username:"charlie",
+    password:"charlie",
+    email:"charlie.io",
+    first_name: 'char',
+    last_name: 'lie',
+    zip_code: 00003
+})
+user4 = User.new({
+    username:"delta",
+    password:"delta",
+    email:"delta.io",
+    first_name: 'del',
+    last_name: 'ta',
+    zip_code: 00004
+})
+user5 = User.new({
+    username:"echo",
+    password:"echo",
+    email:"echo.io",
+    first_name: 'ec',
+    last_name: 'ho',
+    zip_code: 00005
+})
+user6 = User.new({
+    username:"foxtrot",
+    password:"foxtrot",
+    email:"foxtrot.io",
+    first_name: 'fox',
+    last_name: 'trot',
+    zip_code: 00006
+})
+
+user1.save!
+user2.save!
+user3.save!
+user4.save!
+user5.save!
+user6.save!
