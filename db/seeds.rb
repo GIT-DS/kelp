60017bb0a10780d79c57dc65838ca058b809e170
GIT-DS <darrickshin@gmail.com>
2021-09-27 11:24:02 -0700

    Add destroy_all

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+
 #User Seeds
 user1 = User.create!({
     username:"alpha",
